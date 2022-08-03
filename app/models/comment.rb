007eb87c6283a137362ea566305d669beb2ef804
Jerry Owusu <jerryowusu6133@gmail.com>
2022-08-03 11:29:42 +0000

    Add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :author, class_name: 'User'
     belongs_to :post
+
+    validates :author_id, presence: true
+    validates :post_id, presence: true
 end
