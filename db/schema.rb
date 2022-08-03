3485c0ab60bfc6f516c9519ef45703349f6d81bc
Jerry Owusu <jerryowusu6133@gmail.com>
2022-08-03 12:15:52 +0000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_110951) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
-    t.text "bio"
+    t.string "email"
+    t.text "password"
     t.integer "posts_counters", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
