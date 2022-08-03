74cb3af7e94d535103b383ee4a50bb5e96034984
Jerry Owusu <jerryowusu6133@gmail.com>
2022-08-03 12:04:25 +0000

    Update controllers

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::PostsController < ApplicationController
+    def index
+        @user = User.find(params[:user_id])
+        @all_posts = @user.posts.includes(:comments).order(created_at: :desc)
+    end
+    
+      def show
+        @user = User.find(params[:user_id])
+        @post = Post.find(params[:id])
+      end
+    
+      def new
+        @post = Post.new
+        @current_user = current_user
+        
+      end
+    
+      def create
+        @post = current_user.posts.new(post_params)
+        @post.likes_counter = 0
+        @post.comments_counter = 0
+    
+        if @post.save
+          flash[:notice] = 'The post have been created successfully'
+          redirect_to user_posts_path(@post.author_id)
+        else
+          flash[:alert] = 'Adding a new post failed.'
+          render :new, status: :unprocessable_entity
+        end
+      end
+    
+      def destroy
+        post = Post.find(params[:id])
+        user = User.find(post.author_id)
+        user.posts_counter -= 1
+        post.destroy
+        user.save
+        flash[:alert] = 'You have deleted this post successfully!'
+        redirect_to user_posts_path(post.author_id)
+      end
+    
+      def post_params
+        params.require(:post).permit(:author_id, :title, :text).tap do |post_params|
+          post_params.require(:text)
+        end
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
-  resources :posts
-  resources :users
+  
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources :users, only: [:index, :show, :edit, :update] do
+        resources :posts, only: [:index, :new, :show, :create, :destroy]
+      end
+      resources :posts do
+        resources :comments, only: [:create, :destroy]
+        resources :likes, only: [:create]
+      end
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
