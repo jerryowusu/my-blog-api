63959950dfc8bf448bf4a38de241841c319dd6fd
Jerry Owusu <jerryowusu6133@gmail.com>
2022-08-03 15:16:06 +0000

    posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,47 +1,47 @@
 class Api::V1::PostsController < ApplicationController
     def index
-        @user = User.find(params[:user_id])
-        @all_posts = @user.posts.includes(:comments).order(created_at: :desc)
+        @posts = Post.all
+        render json: @posts
     end
     
-      def show
+    def show
         @user = User.find(params[:user_id])
         @post = Post.find(params[:id])
-      end
-    
-      def new
-        @post = Post.new
-        @current_user = current_user
-        
-      end
-    
-      def create
+        render json: @posts
+    end
+       
+    def create
         @post = current_user.posts.new(post_params)
         @post.likes_counter = 0
         @post.comments_counter = 0
     
         if @post.save
-          flash[:notice] = 'The post have been created successfully'
-          redirect_to user_posts_path(@post.author_id)
+          render json: @post, status: :created
         else
           flash[:alert] = 'Adding a new post failed.'
           render :new, status: :unprocessable_entity
         end
-      end
+    end
     
-      def destroy
+    def destroy
         post = Post.find(params[:id])
         user = User.find(post.author_id)
         user.posts_counter -= 1
         post.destroy
         user.save
-        flash[:alert] = 'You have deleted this post successfully!'
-        redirect_to user_posts_path(post.author_id)
-      end
+
+    if @post.destroy   
+        render json: { message: 'Post deleted' }
+    else 
+        render json: { message: 'An error occured, plese try again' }, status: :unprocessable_entity
+    end
+
+      private
     
       def post_params
         params.require(:post).permit(:author_id, :title, :text).tap do |post_params|
           post_params.require(:text)
         end
       end
+    end
 end
